0955528745ba1e6e00a647e7ae1c1aad75e495d8
holocron <holocronster@gmail.com>
2019-06-27 22:06:17 +0200

    procedural

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -1,7 +1,7 @@
 import gdal, osr 
 from skimage.graph import route_through_array
 import numpy as np 
-from geopy.distance import vincenty
+from geopy import distance
 #import matplotlib.pyplot as plt
 import pandas as pd
 import gmplot
@@ -49,28 +49,27 @@ def createPath(costSurfaceArray,startCoord,stopCoord):
 
 #Calculate the vincenty distance starts from the first pair of points to the last.
 def calculateDistance(pathIndices):
-    distance = 0
+    dist = 0
 
     for i in range(0,(len(pathIndices[0])-1)):
-        distance += vincenty((pathIndices[1,i], pathIndices[0,i]), (pathIndices[1,i+1], pathIndices[0,i+1])).miles*0.868976
-    return distance
+        dist += distance.distance((pathIndices[1,i], pathIndices[0,i]), (pathIndices[1,i+1], pathIndices[0,i+1])).km/1.852
+#        dist += distance.vincenty((pathIndices[1,i], pathIndices[0,i]), (pathIndices[1,i+1], pathIndices[0,i+1])).km/1.852
+    return dist
 
-#Test, in this case it's from CATANIA to MANTA
-fromPort = ports.iloc[2205]
-toPort = ports.iloc[1000]
+def routePorts(fromPort, toPort):
+    print(fromPort.Port + "->" + toPort.Port)
+    startCoord = (fromPort.longitude, fromPort.latitude)
+    stopCoord = (toPort.longitude, toPort.latitude)
 
-print(fromPort.Port + "->" + toPort.Port)
-startCoord = (fromPort.longitude, fromPort.latitude)
-stopCoord = (toPort.longitude, toPort.latitude)
+    pathIndices = createPath(mapArray,startCoord,stopCoord)
+    dist = calculateDistance(pathIndices)
 
-pathIndices = createPath(mapArray,startCoord,stopCoord)
-distance = calculateDistance(pathIndices)
+    print("distance is " + str(dist))
 
-print("distance is " + distance)
+    gmap3 = gmplot.GoogleMapPlotter((fromPort.longitude+toPort.longitude)/2, (fromPort.latitude+toPort.latitude)/2, 3)
+    gmap3.plot(pathIndices[0,:], pathIndices[1,:],  
+            'cornflowerblue', edge_width = 2.5) 
+    gmap3.draw( "map-"+fromPort.Port+"-"+toPort.Port+".html") 
 
-gmap3 = gmplot.GoogleMapPlotter(0, 0, 1)
-gmap3.scatter( pathIndices[0,:], pathIndices[1,:], '# FF0000', 
-                              size = 40, marker = False )  
-gmap3.plot(pathIndices[0,:], pathIndices[1,:],  
-           'cornflowerblue', edge_width = 2.5) 
-gmap3.draw( "map13.html" ) 
+routePorts(ports.iloc[2377],ports.iloc[1788])
+routePorts(ports.iloc[2205],ports.iloc[1000])
